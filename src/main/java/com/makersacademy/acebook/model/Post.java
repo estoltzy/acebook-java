16590ec7326b6a19e47beec6f49d13416633c0b4
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-04-25 10:41:26 +0100

    created user_id column in posts table

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -2,6 +2,7 @@ package com.makersacademy.acebook.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
